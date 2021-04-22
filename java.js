88e7d2fb12efc96d9ad388071f88e2ba5581b342
maram121‏ <maram_mohra@hotmail.com>
2021-04-22 09:57:31 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+let x = Number(prompt('Enter first value')) ||0;
+let y = Number(prompt('Enter second value')) ||0;
+if (x >= y)
+alert("The larger number is:"+x );
+ else if(x == y)
+alert("The numbers are equal");
